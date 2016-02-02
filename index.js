437a2c9050aada8b4de2950ced0938c264173671
Doug Shamoo <doug.shamoo@gmail.com>
2016-02-01 18:58:56 -0800

    Add favicon [Closes #4]

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.listen(port, function() {
   console.log('Server listening on port:', port);
